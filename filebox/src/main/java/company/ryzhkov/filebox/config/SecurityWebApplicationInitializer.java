31da048d574181acef19bdcadda1a61edc91e345
StanislavRyzhkov <30403280+StanislavRyzhkov@users.noreply.github.com>
2020-03-17 20:48:24 +0700

    17 03 20

--- filebox/src/main/java/company/ryzhkov/filebox/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package company.ryzhkov.filebox.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- shop_hw6/src/main/java/company/ryzhkov/shop/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package company.ryzhkov.shop.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
